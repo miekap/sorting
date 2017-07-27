057738769fc92424152bccc1cdfe627f32104bf6
Mieka P <git@mieka.com>
2017-07-27 16:15:08 -0400

    finished merge, but not specs

--- mergeSort.js
+++ mergeSort.js
@@ -1,40 +1,40 @@
-function split() {
-	var wholeArray = [1, 2, 3, 4, 5]; 
-	var middle = Math.ceil((wholeArray.length-1)/2); 
-	var firstHalf = wholeArray.slice(0, middle); 
-	var secondHalf = wholeArray.slice(middle); 
+function split(wholeArray) {
+//	var wholeArray = [1, 2, 3, 4, 5];
+	var middle = Math.ceil((wholeArray.length-1)/2);
+	var firstHalf = wholeArray.slice(0, middle);
+	var secondHalf = wholeArray.slice(middle);
   	return [firstHalf, secondHalf];
 }
 
 function merge(leftArr, rightArr) {
-	var result = []; 
-	var leftIndex = 0; 
-	var rightIndex = 0; 
+	var result = [];
+	var leftIndex = 0;
+	var rightIndex = 0;
 
 	while(leftIndex < leftArr.length || rightIndex < rightArr.length) {
 		if(leftIndex === leftArr.length) {
-			result.push(rightArr[rightIndex]); 
-			rightIndex++; 
+			result.push(rightArr[rightIndex]);
+			rightIndex++;
 		} else if(rightIndex === rightArr.length) {
-			result.push(leftArr[leftIndex]); 
-			leftIndex++; 
+			result.push(leftArr[leftIndex]);
+			leftIndex++;
 		}
 		else if(leftArr[leftIndex] < rightArr[rightIndex]) {
-			result.push(leftArr[leftIndex]); 
-			leftIndex++; 
+			result.push(leftArr[leftIndex]);
+			leftIndex++;
 		} else {
-			result.push(rightArr[rightIndex]); 
-			rightIndex++; 
+			result.push(rightArr[rightIndex]);
+			rightIndex++;
 		}
 	}
-	return result; 
+	return result;
 }
-
 
 function mergeSort(array) {
-
-
-  /* your code here */
-
+	if (array.length === 1) {
+		return array;
+	} else {
+		var splits = split(array);
+	  return merge(mergeSort(splits[0]),mergeSort(splits[1]));
+	}
 }
-
