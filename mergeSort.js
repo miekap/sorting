3210753d4f7712c213e16e20f2fc59ed4fd09f0c
puhtreeseeya <puhtreeseeya@gmail.com>
2017-07-27 15:44:14 -0400

    merge and split complete, mergeSplit TBD

---
+++ mergeSort.js
@@ -0,0 +1,40 @@
+function split() {
+	var wholeArray = [1, 2, 3, 4, 5]; 
+	var middle = Math.ceil((wholeArray.length-1)/2); 
+	var firstHalf = wholeArray.slice(0, middle); 
+	var secondHalf = wholeArray.slice(middle); 
+  	return [firstHalf, secondHalf];
+}
+
+function merge(leftArr, rightArr) {
+	var result = []; 
+	var leftIndex = 0; 
+	var rightIndex = 0; 
+
+	while(leftIndex < leftArr.length || rightIndex < rightArr.length) {
+		if(leftIndex === leftArr.length) {
+			result.push(rightArr[rightIndex]); 
+			rightIndex++; 
+		} else if(rightIndex === rightArr.length) {
+			result.push(leftArr[leftIndex]); 
+			leftIndex++; 
+		}
+		else if(leftArr[leftIndex] < rightArr[rightIndex]) {
+			result.push(leftArr[leftIndex]); 
+			leftIndex++; 
+		} else {
+			result.push(rightArr[rightIndex]); 
+			rightIndex++; 
+		}
+	}
+	return result; 
+}
+
+
+function mergeSort(array) {
+
+
+  /* your code here */
+
+}
+
---
+++ mergeSort.spec.js
@@ -0,0 +1,11 @@
+describe('Split Array function', function() {
+  it('is able to split an array into two halves', function() {
+    
+  });
+});
+
+describe('Merge Sort', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    // test the merging algorithm
+  });
+});
